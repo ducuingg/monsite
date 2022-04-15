f5c7f189173c7e1989c76e5c3e5e49c054ea7e07
ducuingg <ducuingg@yahoo.fr>
2022-04-15 15:15:28 +0200

    animations js

--- main.js
+++ main.js
@@ -12,3 +12,46 @@ TL1
 
 TL1.play();
 
+var imgzoom = document.querySelector(".contentphoto img");
+
+window.addEventListener("scroll",()=>{
+
+    
+
+
+    if(window.scrollY>4400){
+        imgzoom.style = "width:50vw "
+    }
+    if(window.scrollY>4450){
+        imgzoom.style = "width:55vw"
+    }
+    if(window.scrollY>4500){
+        imgzoom.style = "width:60vw"; 
+    }
+    if(window.scrollY>4550){
+        imgzoom.style = "width:65vw"; 
+    }
+    if(window.scrollY>4600){
+        imgzoom.style = "width:70vw";  
+    }
+    if(window.scrollY>4650){
+        imgzoom.style = "width:75vw"; 
+    }
+    if(window.scrollY>4700){
+        imgzoom.style = "width:80vw"; 
+    }
+    if(window.scrollY>4750){
+        imgzoom.style = "width:85vw"; 
+    }
+    if(window.scrollY>4800){
+        imgzoom.style = "width:90vw"; 
+    }
+    if(window.scrollY>4850){
+        imgzoom.style = "width:95vw"; 
+    }
+    if(window.scrollY>4900){
+        imgzoom.style = "width:100vw" 
+    }
+});
+
+
